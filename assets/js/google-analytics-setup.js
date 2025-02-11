c5d1bff8886f267775f32a33471961664c500b96
lescobedo <lescobedoai@gmail.com>
2025-02-10 22:56:23 -0500

    google analytics setup reconciled

--- assets/js/google-analytics-setup.js
+++ assets/js/google-analytics-setup.js
@@ -1,10 +1,9 @@
+// google-analytics-setup.js
 window.dataLayer = window.dataLayer || [];
 function gtag() {
   window.dataLayer.push(arguments);
 }
-gtag("js", new Date());
-gtag("config", "{{ site.google_analytics }}", {
+gtag('js', new Date());
+gtag('config', '{{ site.google_analytics }}', {
   'anonymize_ip': true
 });
-
-
